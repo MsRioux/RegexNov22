f8fcb992d3edfff59e272d50b851a9e8c2b85db2
MsRioux <jrioux@mvusd.net>
2022-11-17 12:07:40 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student janeth = new Student("Janeth", 600, 7);
+        System.out.println(janeth);
+        janeth.setName("9E");
+        System.out.println(janeth.getName());
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,68 @@
+public class Student
+{
+    private String name;
+    private int height;// in inches
+    private int age;
+    private String SSN;
+
+    public Student(String name, int height, int age) {
+        setName(name);
+        this.height = height;
+        this.age = age;
+    }
+    public Student ()
+    {
+        name = "";
+        height = 0;
+        age = 0;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(!name.matches("[A-Z][a-zA-Z]*"))
+        {
+//            System.out.println("NOPE!");
+            this.name = "Reginald";
+        }
+        else
+        {
+            this.name = name;
+        }
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+    //SSN format is  3 digits, dash, 2 digits, dash, 4 digits
+    public void setSSN(String SSN)
+    {
+        if(SSN.matches("[0-9]{3}-[0-9]{2}-[0-9]{4}")) {
+            this.SSN = SSN;
+        }
+        else {
+            this.SSN = "Please Try Again";
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", age=" + age +
+                '}';
+    }
+}
